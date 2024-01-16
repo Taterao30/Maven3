5514a426e4c1c9ec1bbd43e186517fe355ff63c6
TatetraoG <tateraog30@gmail.com>
2024-01-16 11:24:51 +0530

    Delhi

--- src/test/java/Tutorial1/Demo1.java
+++ src/test/java/Tutorial1/Demo1.java
@@ -3,6 +3,11 @@ package Tutorial1;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		m1();
+	}
+	public static void m1()
+	{
+		System.out.println("Delhi");
 	}
 
 }
